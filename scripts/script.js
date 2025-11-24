89874e393d0e8935bec3e0ecac54031f3d3a79c0
M. K. Hamim <mkhamimbd@gmail.com>
2025-11-24 11:22:08 +0600

    organized the javascript code

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 const todoList = document.querySelector('.todoList');
-    const todoArray = [];
+const todoArray = [];
 
-    renderTodoList();
+renderTodoList();
 
-    function renderTodoList() {
-      let todoArrayHTML = '';
+function renderTodoList() {
+  let todoArrayHTML = '';
 
-      for (let i = 0; i < todoArray.length; i++) {
-        const todo = todoArray[i];
-        const html = `
+  for (let i = 0; i < todoArray.length; i++) {
+    const todo = todoArray[i];
+    const html = `
         <p>
           ${todo} 
           <button onclick="
@@ -17,31 +17,31 @@ const todoList = document.querySelector('.todoList');
           ">Delete</button>
         </p>
           `;
-        todoArrayHTML += html;
-      }
-      console.log(todoArrayHTML);
-      todoList.innerHTML = todoArrayHTML;
-    }
+    todoArrayHTML += html;
+  }
+  console.log(todoArrayHTML);
+  todoList.innerHTML = todoArrayHTML;
+}
 
-    function addTodo() {
-      const todoInput = document.querySelector('.todo-input');
-      const inputValue = todoInput.value;
+function addTodo() {
+  const todoInput = document.querySelector('.todo-input');
+  const inputValue = todoInput.value;
 
-      if (inputValue === '**clearAll**') {
-        todoArray = [];
-        console.log(todoArray);
-      } else {
-        todoArray.push(inputValue);
-        console.log(todoArray);
-      }
+  if (inputValue === '**clearAll**') {
+    todoArray = [];
+    console.log(todoArray);
+  } else {
+    todoArray.push(inputValue);
+    console.log(todoArray);
+  }
 
-      todoInput.value = '';
+  todoInput.value = '';
 
-      renderTodoList();
-    }
+  renderTodoList();
+}
 
-    function addTodoOnEnter() {
-      if (event.key === 'Enter') {
-        addTodo();
-      }
-    }
+function addTodoOnEnter() {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+}
