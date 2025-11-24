40c3aa455da362b623c0963dfd858490f30f723f
M. K. Hamim <mkhamimbd@gmail.com>
2025-11-24 11:20:41 +0600

    seperated javascript codes to its own .js file & linked it to index.html

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+const todoList = document.querySelector('.todoList');
+    const todoArray = [];
+
+    renderTodoList();
+
+    function renderTodoList() {
+      let todoArrayHTML = '';
+
+      for (let i = 0; i < todoArray.length; i++) {
+        const todo = todoArray[i];
+        const html = `
+        <p>
+          ${todo} 
+          <button onclick="
+            todoArray.splice(${i}, 1);
+            renderTodoList();
+          ">Delete</button>
+        </p>
+          `;
+        todoArrayHTML += html;
+      }
+      console.log(todoArrayHTML);
+      todoList.innerHTML = todoArrayHTML;
+    }
+
+    function addTodo() {
+      const todoInput = document.querySelector('.todo-input');
+      const inputValue = todoInput.value;
+
+      if (inputValue === '**clearAll**') {
+        todoArray = [];
+        console.log(todoArray);
+      } else {
+        todoArray.push(inputValue);
+        console.log(todoArray);
+      }
+
+      todoInput.value = '';
+
+      renderTodoList();
+    }
+
+    function addTodoOnEnter() {
+      if (event.key === 'Enter') {
+        addTodo();
+      }
+    }
